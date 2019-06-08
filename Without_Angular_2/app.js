28c9a4989ded44e49963ce804868f8579fc0960e
MKA1500 <maciejkabsch@gmail.com>
2019-06-08 23:45:40 +0200

    new app example

--- Without_Angular_2/app.js
+++ Without_Angular_2/app.js
@@ -1,4 +1,28 @@
-const inputCourseName = document.querySelector('#inputCourseName');
-const inputCourseRating = document.querySelector('#inputCourseRating');
+const courseNameInput = document.querySelector('#inputCourseName');
+const courseRatingInput = document.querySelector('#inputCourseRating');
 const btnAdd = document.querySelector('#btnAdd');
 const coursesList = document.querySelector('#coursesList');
+const alertController = document.querySelector('ion-alert-controller');
+
+btnAdd.addEventListener('click', () => {
+  const enteredName = courseNameInput.value;
+  const enteredRating = courseRatingInput.value;
+
+  if (
+    enteredName.trim().length <= 0 ||
+    enteredRating.trim().length <= 0 ||
+    enteredRating < 1 ||
+    enteredRating > 5
+  ) {
+    alertController.create({});
+    return;
+  }
+
+  const newItem = document.createElement('ion-item');
+  newItem.innerHTML = `<strong>${enteredName}:</strong> &nbsp;${enteredRating}/5`;
+
+  coursesList.appendChild(newItem);
+
+  courseNameInput.value = '';
+  courseRatingInput.value = '';
+});
