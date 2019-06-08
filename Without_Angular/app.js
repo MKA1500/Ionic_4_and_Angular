91a91e2fd08bebcfc25a298ba222a81edfdff4fb
MKA1500 <maciejkabsch@gmail.com>
2019-06-08 14:52:51 +0200

    basic app with alert

--- Without_Angular/app.js
+++ Without_Angular/app.js
@@ -2,7 +2,43 @@ const inputExpense = document.querySelector('#inputExpense');
 const inputAmount = document.querySelector('#inputAmount');
 const btnClear = document.querySelector('#btnClear');
 const btnConfirm = document.querySelector('#btnConfirm');
+const expensesList = document.querySelector('#expensesList');
+const totalExpensesOutput = document.querySelector('#totalExpenses');
+const alertController = document.querySelector('ion-alert-controller');
+
+let totalExpenses = 0;
+
+const clear = () => {
+  inputExpense.value = '';
+  inputAmount.value = '';
+};
 
 btnConfirm.addEventListener('click', () => {
-  console.log('confirm');
+  const enteredExpense = inputExpense.value;
+  const enteredAmount = inputAmount.value;
+
+  if (
+    enteredExpense.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    alertController.create({
+      message: "Please enter valid expense and amount!",
+      header: "Invalid inputs",
+      buttons: ['OK']
+    }).then(alertElement => {
+      alertElement.present();
+    });
+    return;
+  }
+  console.log(enteredExpense, enteredAmount);
+
+  const newItem = document.createElement('ion-item');
+  newItem.textContent = enteredExpense + ': $' + enteredAmount;
+  expensesList.appendChild(newItem);
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+  clear();
 });
+
+btnClear.addEventListener('click', clear);
