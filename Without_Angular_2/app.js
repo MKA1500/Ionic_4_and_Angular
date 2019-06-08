e3bc57b095daf5c2694cb22d83f00f62bceb332a
MKA1500 <maciejkabsch@gmail.com>
2019-06-08 17:40:55 +0200

    new example

---
+++ Without_Angular_2/app.js
@@ -0,0 +1,4 @@
+const inputCourseName = document.querySelector('#inputCourseName');
+const inputCourseRating = document.querySelector('#inputCourseRating');
+const btnAdd = document.querySelector('#btnAdd');
+const coursesList = document.querySelector('#coursesList');
