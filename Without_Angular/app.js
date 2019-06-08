3794ca864d6198103189dcbebca8376543db222a
MKA1500 <maciejkabsch@gmail.com>
2019-06-08 12:08:01 +0200

    adding js

---
+++ Without_Angular/app.js
@@ -0,0 +1,8 @@
+const inputExpense = document.querySelector('#inputExpense');
+const inputAmount = document.querySelector('#inputAmount');
+const btnClear = document.querySelector('#btnClear');
+const btnConfirm = document.querySelector('#btnConfirm');
+
+btnConfirm.addEventListener('click', () => {
+  console.log('confirm');
+});
